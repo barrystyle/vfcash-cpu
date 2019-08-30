00c3445e03658974346acfd50a061b5994a0d2a3
James <James@voxdsp.com>
2019-08-30 23:22:18 +0100

    general

--- compile.sh
+++ compile.sh
@@ -1,2 +1,4 @@
+apt install --assume-yes mingw-w64
+apt install --assume-yes gcc
 gcc -Ofast -fopenmp miner.c ecc.c base58.c -lm -o miner-linux-64
 i686-w64-mingw32-gcc -Ofast -fopenmp miner.c ecc.c base58.c -lm -o miner-win-64.exe
