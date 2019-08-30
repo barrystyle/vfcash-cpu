2b2849db3ab3b9318b47b2c153d89012cde41b4d
James <James@voxdsp.com>
2019-08-30 22:37:09 +0100

    general

--- compile.sh
+++ compile.sh
@@ -1,1 +1,2 @@
-gcc -Ofast -fopenmp miner.c ecc.c base58.c -lm -o miner
+gcc -Ofast -fopenmp miner.c ecc.c base58.c -lm -o miner-linux-64
+i686-w64-mingw32-gcc -Ofast -fopenmp miner.c ecc.c base58.c -lm -o miner-win-64.exe
