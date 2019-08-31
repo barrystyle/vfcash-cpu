dfc1ae023aa472acf5b777a02eeef7eb734d9a85
James <James@voxdsp.com>
2019-08-31 18:09:51 +0100

    general

--- compile.sh
+++ compile.sh
@@ -6,12 +6,12 @@ apt install --assume-yes mingw-w64
 apt install --assume-yes gcc
 apt install --assume-yes clang
 
-gcc -Ofast -fopenmp miner.c ecc.c base58.c -lm -o gcc-miner-linux-64
-clang -Ofast -fopenmp miner.c ecc.c base58.c -lm -o clang-miner-linux-64
-i686-w64-mingw32-gcc -Ofast -fopenmp miner.c ecc.c base58.c -lm -o miner-win-64.exe
+gcc -Ofast -fopenmp miner.c ecc.c base58.c -lm -o bin/gcc-miner-linux-64
+clang -Ofast -fopenmp miner.c ecc.c base58.c -lm -o bin/clang-miner-linux-64
+i686-w64-mingw32-gcc -Ofast -fopenmp miner.c ecc.c base58.c -lm -o bin/miner-win-64.exe
 
-gcc -Ofast -fopenmp gminer.c sha3.c ecc.c base58.c -lSDL2 -lSDL2_net -lm -o gcc-gui-miner-linux-64
-clang -Ofast -fopenmp gminer.c sha3.c ecc.c base58.c -lSDL2 -lSDL2_net -lm -o clang-gui-miner-linux-64
-i686-w64-mingw32-gcc -Ofast -fopenmp gminer.c sha3.c ecc.c base58.c -lSDL2 -lSDL2_net -lm -o gui-miner-win-64.exe
+gcc -Ofast -fopenmp gminer.c sha3.c ecc.c base58.c -lSDL2 -lSDL2_net -lm -o bin/gcc-gui-miner-linux-64
+clang -Ofast -fopenmp gminer.c sha3.c ecc.c base58.c -lSDL2 -lSDL2_net -lm -o bin/clang-gui-miner-linux-64
+i686-w64-mingw32-gcc -Ofast -fopenmp gminer.c sha3.c ecc.c base58.c -lSDL2 -lSDL2_net -lm -o bin/gui-miner-win-64.exe
 
 
