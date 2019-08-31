1d2736e3a4f26ad820ad3279b9e786fd46d9f69f
James <James@voxdsp.com>
2019-08-31 02:25:19 +0100

    general

--- compile.sh
+++ compile.sh
@@ -1,4 +1,14 @@
+apt install --assume-yes libsdl2-net-2.0-0
+apt install --assume-yes libsdl2-net-dev
+apt install --assume-yes libsdl2-2.0-0
+apt install --assume-yes libsdl2-dev
 apt install --assume-yes mingw-w64
 apt install --assume-yes gcc
+
 gcc -Ofast -fopenmp miner.c ecc.c base58.c -lm -o miner-linux-64
 i686-w64-mingw32-gcc -Ofast -fopenmp miner.c ecc.c base58.c -lm -o miner-win-64.exe
+
+gcc -Ofast -fopenmp gminer.c ecc.c base58.c -lSDL2 -lm -o gui-miner-linux-64
+i686-w64-mingw32-gcc -Ofast -fopenmp gminer.c ecc.c base58.c -lSDL2 -lm -o gui-miner-win-64.exe
+
+
--- gminer.c
+++ gminer.c
@@ -1,30 +1,16 @@
-//VF CASH - Standalone Miner - August 2019
-//James William Fletcher
-
 #include <omp.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_net.h>
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
-#include <string.h>
-#include <stdlib.h>
 
 #include "ecc.h"
 #include "base58.h"
 
-float approx_sqrt(float n)
-{
-    long i;
-    float x2, y;
-    const float threehalfs = 1.5F;
-    x2 = n * 0.5F;
-    y  = n;
-    i  = * ( long * ) &y;
-    i  = 0x5f3759df - ( i >> 1 );
-    y  = * ( float * ) &i;
-    y  = y * ( threehalfs - ( x2 * y * y ) );
-    y  = y * ( threehalfs - ( x2 * y * y ) );
-    return n*y;
-}
+SDL_Window* window;
+const int width = 320;
+const int height = 120;
 
 double toDB(const uint64_t b)
 {
@@ -55,13 +41,6 @@ double gNa(const vec3* a, const vec3* b)
         return 1;
 
     return dot / (m1*m2);
-}
-
-double getMiningDifficulty()
-{
-    const time_t lt = time(0);
-    const struct tm* tmi = gmtime(&lt);
-    return (double)(tmi->tm_hour+1) * 0.01;
 }
 
 uint64_t isSubGenesisAddress(uint8_t *a)
@@ -167,67 +146,173 @@ double subDiff(uint8_t *a)
     return diff;
 }
 
-
-int main()
+void mine()
 {
-    printf("Please wait, minted keys are saved to minted.txt, difficulty 0.24 ...\n");
+  uint8_t priv[ECC_BYTES];
+  uint8_t pub[ECC_BYTES+1];
+  ecc_make_key(pub, priv);
+  uint64_t r = isSubGenesisAddress(pub);
+  if(r != 0)
+  {
+    char bpriv[256];
+    memset(bpriv, 0, sizeof(bpriv));
+    size_t len = 256;
+    b58enc(bpriv, &len, priv, ECC_BYTES);
+
+    const double diff = subDiff(pub);
+    const double fr = toDB(r);
+    printf("Private Key: %s (%.3f DIFF) (%.3f VFC)\n\n", bpriv, diff, fr);
     
-    #pragma omp parallel
-    //#pragma omp target teams distribute parallel for
-    //for(int i=0; i < 2048; ++i) 
-    while(1)
+    FILE* f = fopen("minted.txt", "a");
+    if(f != NULL)
     {
-        //i=0;
-        int tid = omp_get_thread_num();
-        int nthreads;
-        if(tid == 0)
-        {
-            nthreads = omp_get_num_threads();
-            printf("Number of threads: %d\n", nthreads);
-        }
-        
-        time_t nt = time(0)+16;
-        uint64_t c = 0;
-        while(1)
-        {
-            if(time(0) > nt)
-            {
-                if(tid == 0)
-                    printf("HASH/s: %lu\n", (c*nthreads)/16);
+      fprintf(f, "%s / %.3f / %.3f\n", bpriv, diff, fr);
+      fclose(f);
+    }
+  }
+}
 
-                if(c > 0)
-                    printf("T-%i: %lu\n", omp_get_thread_num(), c/16);
-
-                c = 0;
-                nt = time(0)+16;
-            }
-
-            uint8_t priv[ECC_BYTES];
-            uint8_t pub[ECC_BYTES+1];
-            ecc_make_key(pub, priv);
-            uint64_t r = isSubGenesisAddress(pub);
-            if(r != 0)
-            {
-                char bpriv[256];
-                memset(bpriv, 0, sizeof(bpriv));
-                size_t len = 256;
-                b58enc(bpriv, &len, priv, ECC_BYTES);
-
-                const double diff = subDiff(pub);
-                const double fr = toDB(r);
-                printf("Private Key: %s (%.3f DIFF) (%.3f VFC)\n\n", bpriv, diff, fr);
-                
-                FILE* f = fopen("minted.txt", "a");
-                if(f != NULL)
-                {
-                    fprintf(f, "%s / %.3f / %.3f\n", bpriv, diff, fr);
-                    fclose(f);
-                }
-            }
-
-            c++;
-        }
+long double getCPULoad()
+{
+    long double a[4], b[4];
+    char dump[50];
+    
+    FILE *fp = fopen("/proc/stat","r");
+    if(fp)
+    {
+        fscanf(fp,"%*s %Lf %Lf %Lf %Lf",&a[0],&a[1],&a[2],&a[3]);
+        fclose(fp);
     }
     
-    return 0;
+    SDL_Delay(1000);
+
+    fp = fopen("/proc/stat","r");
+    if(fp)
+    {
+        fscanf(fp,"%*s %Lf %Lf %Lf %Lf",&b[0],&b[1],&b[2],&b[3]);
+        fclose(fp);
+    }
+
+    return ((b[0]+b[1]+b[2]) - (a[0]+a[1]+a[2])) / ((b[0]+b[1]+b[2]+b[3]) - (a[0]+a[1]+a[2]+a[3]));
+}
+
+void setpixel(SDL_Surface * surface, const int x, const int y, const Uint8 r, const Uint8 g, const Uint8 b)
+{
+  if(x < 0 || x > width || y < 0 || y > height)
+    return;
+  const Uint32 color = SDL_MapRGB(surface->format, r, g, b);
+  Uint8 * pixel = (Uint8*)surface->pixels;
+  pixel += (y * surface->pitch) + (x * sizeof(Uint32));
+  *((Uint32*)pixel) = color;
+}
+
+void line(SDL_Surface * surface, int x0, int y0, int x1, int y1, const Uint8 r, const Uint8 g, const Uint8 b)
+{
+  int dx = abs(x1-x0), sx = x0<x1 ? 1 : -1;
+  int dy = abs(y1-y0), sy = y0<y1 ? 1 : -1; 
+  int err = (dx>dy ? dx : -dy)/2, e2;
+  while(1)
+  {
+    setpixel(surface, x0, y0, r, g, b);
+    if(x0 == x1 && y0 == y1){break;}
+    e2 = err;
+    if(e2 >-dx){err -= dy;x0 += sx;}
+    if(e2 < dy){err += dx;y0 += sy;}
+  }
+}
+
+int ool = 0;
+int lv = 0;
+void render(SDL_Surface* surface)
+{
+  const int off = (float)(getCPULoad()*height);
+  
+  if(lv != 0)
+    line(surface, ool-3, height - lv, ool, height - off, 255, 191, 0);
+  //setpixel(surface, ool, height - off, 255, 191, 0);
+
+  ool += 3;
+  lv = off;
+  if(ool > width)
+  {
+    ool = 0;
+    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0, 0, 0));
+  }
+}
+
+int main(int argc, char* args[])
+{
+  //Init SDL
+  if(SDL_Init(SDL_INIT_VIDEO) < 0)
+  {
+    fprintf(stderr, "ERROR: SDL_Init(): %s\n", SDL_GetError());
+    return 1;
+  }
+
+  //Create window
+  window = SDL_CreateWindow("vfcminergui", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN);
+  if(window == NULL)
+  {
+    fprintf(stderr, "ERROR: SDL_CreateWindow(): %s\n", SDL_GetError());
+    return 1;
+  }
+  SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
+
+  //Run the miner !
+  #pragma omp parallel
+  while(1)
+  {
+    int tid = omp_get_thread_num();
+    int nthreads;
+    if(tid == 0)
+    {
+        nthreads = omp_get_num_threads();
+        printf("Number of threads: %d\n", nthreads);
+    }
+    
+    time_t nt = time(0);
+    uint64_t c = 0;
+    while(1)
+    {
+      if(tid == 0) //UI Thread
+      {
+        SDL_Event event;
+        while(SDL_PollEvent(&event))
+        {
+          switch(event.type)
+          {
+            case SDL_QUIT:
+            {
+              exit(0);
+            }
+            break;
+          }
+        }
+      }
+
+      //Update every 3 seconds
+      if(tid == 0 && time(0) > nt)
+      {
+        char title[256];
+        sprintf(title, "H/s: %lu / C: %d", (c*nthreads), nthreads);
+        SDL_SetWindowTitle(window, title);
+
+        render(screenSurface);
+        SDL_UpdateWindowSurface(window);
+
+        c = 0;
+        nt = time(0);
+      }
+
+      //Mine for a key
+      mine();
+      c++;
+    }
+  }
+
+
+  //Done.
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
 }
