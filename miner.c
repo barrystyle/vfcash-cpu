2fe8d425b799ec9c2356003e84039d07d2a0e5fa
Luke <luke@clashproject.com>
2019-09-20 22:10:10 +0200

    Minor updates
    Todo: Check if it works, bugfixes, uint64t vector to array

--- miner.c
+++ miner.c
@@ -20,34 +20,49 @@ uint8_t rpriv[ECC_BYTES];
 uint8_t rpub[ECC_BYTES+1];
 
 double toDB(const uint64_t b)
-{
+{ // Do something about this.
     return (double)(b) / 1000;
 }
 
-double mfloor(double i)
-{
-    if(i < 0)
-        return (int)i - 1;
-    else
-        return (int)i;
-}
+#define mfloor(x) floor(x) //It's already implemented in math
 
 struct vec3
 {
-    uint16_t x,y,z;
+    uint64_t x,y,z;
 };
 typedef struct vec3 vec3;
 
 double gNa(const vec3* a, const vec3* b)
 {
-    const double dot = ((double)(a->x) * (double)(b->x)) + ((double)(a->y) * (double)(b->y)) + (double)((a->z) * (double)(b->z));
-    const double m1 = sqrt((double)((a->x) * (double)(a->x)) + (double)((a->y) * (double)(a->y)) + (double)((a->z) * (double)(a->z)));
-    const double m2 = sqrt((double)((b->x) * (double)(b->x)) + (double)((b->y) * (double)(b->y)) + (double)((b->z) * (double)(b->z)));
-
-    if((m1 == 0 && m2 == 0) || dot == 0)
+    // In-Place multiplaction is better than consting it
+    // Using 64bit multiplication to optimise it for CPU.
+    // If it is supposed to be optimised for GPU, chose 32bit
+    // floating point operations instead.
+    uint64_t tmp0_0 = ((a->x) * (b->x));
+    const uint64_t tmp0_1 = ((a->y) * (b->y));
+    const uint64_t tmp0_2 = ((a->z) * (b->z));
+    uint64_t tmp1_0 = ((a->x) * (a->x));
+    const uint64_t tmp1_1 = ((a->y) * (a->y));
+    const uint64_t tmp1_2 = ((a->z) * (a->z));
+    uint64_t tmp2_0 = ((b->x) * (b->x));
+    const uint64_t tmp2_1 = ((b->y) * (b->y));
+    const uint64_t tmp2_2 = ((b->z) * (b->z));
+    tmp0_0 = tmp0_0+tmp0_1+tmp0_2; //tmp0_0 = dot
+    tmp1_0 = tmp1_0+tmp1_1+tmp1_2;
+    tmp2_0 = tmp2_0+tmp2_1+tmp2_2;
+    const double m1 = sqrt(tmp1_0);
+    const double m2 = sqrt(tmp2_0);
+    // New variable as multiplication takes as much
+    // time as another comparision. However, an &&
+    // is saved as well as a multiplication in case
+    // the if below is not true.
+    const double m3 = m1*m2;
+    // If either is zero, the algorithm fails.
+    // A check for that has to be done, not a check
+    // whether _both_ are.
+    if(!tmp0_0 || (m3==0))
         return 1;
-
-    return dot / (m1*m2);
+    return dot/m3;
 }
 
 //////////////////////////////////////////////////////////////////////////////
@@ -59,54 +74,44 @@ uint64_t isSubGenesisAddress(uint8_t *a)
     vec3 v[5];
 
     uint8_t *ofs = a;
-    fastcpy(&v[0].x, ofs);
-    fastcpy(&v[0].y, ofs + sizeof(uint16_t));
-    fastcpy(&v[0].z, ofs + (sizeof(uint16_t)*2));
+    fastcpy(&v[0].x, ofs    );
+    fastcpy(&v[0].y, ofs + 2);
+    fastcpy(&v[0].z, ofs + 4));
+    fastcpy(&v[1].x, ofs + 6);
+    fastcpy(&v[1].y, ofs + 8);
+    fastcpy(&v[1].z, ofs +10);
+    fastcpy(&v[2].x, ofs +12);
+    fastcpy(&v[2].y, ofs +14);
+    fastcpy(&v[2].z, ofs +16);
+    fastcpy(&v[3].x, ofs +18);
+    fastcpy(&v[3].y, ofs +20);
+    fastcpy(&v[3].z, ofs +22);
+    fastcpy(&v[4].x, ofs +24);
+    fastcpy(&v[4].y, ofs +26);
+    fastcpy(&v[4].z, ofs +28);
 
-    ofs = ofs + (sizeof(uint16_t)*3);
-    fastcpy(&v[1].x, ofs);
-    fastcpy(&v[1].y, ofs + sizeof(uint16_t));
-    fastcpy(&v[1].z, ofs + (sizeof(uint16_t)*2));
-
-    ofs = ofs + (sizeof(uint16_t)*3);
-    fastcpy(&v[2].x, ofs);
-    fastcpy(&v[2].y, ofs + sizeof(uint16_t));
-    fastcpy(&v[2].z, ofs + (sizeof(uint16_t)*2));
-
-    ofs = ofs + (sizeof(uint16_t)*3);
-    fastcpy(&v[3].x, ofs);
-    fastcpy(&v[3].y, ofs + sizeof(uint16_t));
-    fastcpy(&v[3].z, ofs + (sizeof(uint16_t)*2));
-
-    ofs = ofs + (sizeof(uint16_t)*3);
-    fastcpy(&v[4].x, ofs);
-    fastcpy(&v[4].y, ofs + sizeof(uint16_t));
-    fastcpy(&v[4].z, ofs + (sizeof(uint16_t)*2));
-
-    const double a1 = gNa(&v[0], &v[3]);
+    const double a1 = gNa(v,     &v[3]);
     const double a2 = gNa(&v[3], &v[2]);
     const double a3 = gNa(&v[2], &v[1]);
     const double a4 = gNa(&v[1], &v[4]);
 
-    const double min = 0.24;
+    #define min 0.24
     
-    if(a1 < min) { if(a2 < min) { if(a3 < min) { if(a4 < min) {
-        const double at = (a1+a2+a3+a4);
-        if(at <= 0)
-            return 0;
+    const double at = a1+a2+a3+a4;
+    if (at<=0) return 0;
+    if(a1 < min && a2 < min && a3 < min && a4 < min) {
         const double ra = at/4;
-        const double mn = 4.166666667;
+        #define mn 4.166666667
         const uint64_t rv = (uint64_t)mfloor(( 1000 + ( 10000*(1-(ra*mn)) ) )+0.5);
         
         printf("\nsubG: %.8f - %.8f - %.8f - %.8f - %.3f VFC < %.3f\n", a1, a2, a3, a4, toDB(rv), ra);
         
         return rv;
-    }}}}
+    }
     
-    const double soft = 0.1;
-    if(a1 < min+soft && a2 < min+soft && a3 < min+soft && a4 < min+soft)
+    #define soft 0.34 //min +0.1
+    if(a1 < soft && a2 < soft && a3 < soft && a4 < soft)
         printf("x: %.8f - %.8f - %.8f - %.8f\n", a1, a2, a3, a4);
-
     return 0;
 }
 
@@ -115,31 +120,23 @@ double subDiff(uint8_t *a)
     vec3 v[5]; //Vectors
 
     uint8_t *ofs = a;
-    fastcpy(&v[0].x, ofs);
-    fastcpy(&v[0].y, ofs + sizeof(uint16_t));
-    fastcpy(&v[0].z, ofs + (sizeof(uint16_t)*2));
+    fastcpy(&v[0].x, ofs    );
+    fastcpy(&v[0].y, ofs + 2);
+    fastcpy(&v[0].z, ofs + 4));
+    fastcpy(&v[1].x, ofs + 6);
+    fastcpy(&v[1].y, ofs + 8);
+    fastcpy(&v[1].z, ofs +10);
+    fastcpy(&v[2].x, ofs +12);
+    fastcpy(&v[2].y, ofs +14);
+    fastcpy(&v[2].z, ofs +16);
+    fastcpy(&v[3].x, ofs +18);
+    fastcpy(&v[3].y, ofs +20);
+    fastcpy(&v[3].z, ofs +22);
+    fastcpy(&v[4].x, ofs +24);
+    fastcpy(&v[4].y, ofs +26);
+    fastcpy(&v[4].z, ofs +28);
 
-    ofs = ofs + (sizeof(uint16_t)*3);
-    fastcpy(&v[1].x, ofs);
-    fastcpy(&v[1].y, ofs + sizeof(uint16_t));
-    fastcpy(&v[1].z, ofs + (sizeof(uint16_t)*2));
-
-    ofs = ofs + (sizeof(uint16_t)*3);
-    fastcpy(&v[2].x, ofs);
-    fastcpy(&v[2].y, ofs + sizeof(uint16_t));
-    fastcpy(&v[2].z, ofs + (sizeof(uint16_t)*2));
-
-    ofs = ofs + (sizeof(uint16_t)*3);
-    fastcpy(&v[3].x, ofs);
-    fastcpy(&v[3].y, ofs + sizeof(uint16_t));
-    fastcpy(&v[3].z, ofs + (sizeof(uint16_t)*2));
-
-    ofs = ofs + (sizeof(uint16_t)*3);
-    fastcpy(&v[4].x, ofs);
-    fastcpy(&v[4].y, ofs + sizeof(uint16_t));
-    fastcpy(&v[4].z, ofs + (sizeof(uint16_t)*2));
-
-    const double a1 = gNa(&v[0], &v[3]);
+    const double a1 = gNa(v,     &v[3]);
     const double a2 = gNa(&v[3], &v[2]);
     const double a3 = gNa(&v[2], &v[1]);
     const double a4 = gNa(&v[1], &v[4]);
